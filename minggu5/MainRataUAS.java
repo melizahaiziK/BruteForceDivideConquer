9098eb50226c38f99ea599a05e1ae1e0153a7caf
melizahaiziK <melizaha1zik@gmail.com>
2026-03-29 11:47:38 +0700

    Tugas no.3

---
+++ minggu5/MainRataUAS.java
@@ -0,0 +1,9 @@
+package minggu5;
+public class MainRataUAS {
+    public static void main(String[] args) {
+        RataUAS data = new RataUAS();
+        double hasil = data.hitungRata();
+
+        System.out.println("Rata-rata UAS adalah: " + hasil);
+    }
+}
